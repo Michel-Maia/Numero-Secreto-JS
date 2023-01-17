8136c2be6c20e3516559842a9b5e5e2cf94502e6
Michel Maia <programador.michel.maia@gmail.com>
2023-01-17 07:26:05 -0300

    commit

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -25,4 +25,7 @@ function exibeChuteNaTela(chute) {
 }
 
 
+recognition.addEventListener('end', () => recognition.start())
+
+
 
--- app/validacao.js
+++ app/validacao.js
@@ -3,20 +3,32 @@ function  verificaSeChutePossuiValorValido(chute) {
 
     if(chuteForInvalido(numero)) {
         elementoChute.innerHTML += '<div> Valor inválido</div>'
+        return
     }
 
     if(numeroMaiorOuMenorPermitido(numero)){
         elementoChute.innerHTML += `
         <div> Valor inválido: O número secreto está entre ${maiorValor} e ${menorValor}
         </div>`
+        return
     }
 
     if (numero === numeroSecreto) {
         document.body.innerHTML = `
         <h2>Você acertou! </h2>
         <h3>O número secreto era ${numeroSecreto} </h3>
+        <button id="jogar-novamente" class="btn-jogar"> JOgar novamente </button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-sharp fa-solid fa-arrow-down"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-sharp fa-solid fa-arrow-up"></i></div>
         `
     }
+
 
 }
 
@@ -31,7 +43,11 @@ function numeroMaiorOuMenorPermitido(numero) {
 }
 
 
-
+document.body.addEventListener('click', e => {
+    if(e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
 
 
 
