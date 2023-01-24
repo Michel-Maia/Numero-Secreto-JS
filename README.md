e0d469279ec9a4aa69b53f37bb9898ba7acf5853
Michel Maia <programador.michel.maia@gmail.com>
2023-01-24 17:09:48 -0300

    commit

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Numero Secreto JS
- desenvolvido para aprendizado
+
+![preview](./.github/preview.png)
 
 
-ref
-https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API
+Desenvolvido para aprendizado. 17/01/2023
+Realizado durante aula de JavaScript onde foi feito validações e reconhecimento de voz. 
+
+
+[🔗 Clique aqui para acessar] (https://michel-maia.github.io/Numero-Secreto-JS/)
+
+
+## 💻 Tecnologias
+
+- HTML
+- CSS
+- JavaScript 
+
+
+## Referência
+
+- [alura] [https://www.alura.com.br/]
+- [MDN] [https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API]
 
--- app/validacao.js
+++ app/validacao.js
@@ -2,8 +2,18 @@ function  verificaSeChutePossuiValorValido(chute) {
     const numero = +chute
 
     if(chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div> Valor inválido</div>'
-        return
+        if (chute.toUpperCase() === "GAME OVER") {
+            document.body.innerHTML = 
+            `
+            <h2> Game Over ! </h2>
+            <h3> Pressione o botão para jogar novamente </h3>
+            <button id="jogar-novamente" class="btn-jogar"> Jogar Novamente </button>
+            `
+        } else {
+            elementoChute.innerHTML += '<div> Valor inválido</div>'
+            return 
+        }
+       
     }
 
     if(numeroMaiorOuMenorPermitido(numero)){
